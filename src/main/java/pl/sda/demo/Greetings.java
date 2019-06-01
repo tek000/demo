11f247362232416ec2871140e7473eaeaf4ec6ef
Mariusz Kowalczuk <lordmer@gmail.com>
2019-06-01 15:12:47 +0200

    zad 3 test passed

--- src/main/java/pl/sda/demo/Greetings.java
+++ src/main/java/pl/sda/demo/Greetings.java
@@ -8,8 +8,11 @@ public class Greetings {
     public String greet(String name){
         if (name == null){
             return "Witaj, mój przyjacielu";
-
         }
+        if (!name.equals(name.toLowerCase())){
+            return "Witaj " + name.toUpperCase() + " !";
+        }
+
         return "Witaj " + name ;
     }
 }
