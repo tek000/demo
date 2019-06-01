71824ed5d47772644e1816e4bbc0f8101e1ea819
Mariusz Kowalczuk <lordmer@gmail.com>
2019-06-01 15:32:03 +0200

    working

--- src/main/java/pl/sda/demo/Greetings.java
+++ src/main/java/pl/sda/demo/Greetings.java
@@ -5,14 +5,19 @@ package pl.sda.demo;
  */
 public class Greetings {
 
-    public String greet(String name){
-        if (name == null){
-            return "Witaj, mój przyjacielu";
-        }
-        if (!name.equals(name.toLowerCase())){
-            return "WITAJ " + name.toUpperCase() + "!";
-        }
+    public String greet(String name) {
 
-        return "Witaj " + name ;
+        if (name == null) {
+            return "Witaj, mój przyjacielu";
+
+
+        }
+        if (name.contains(",")) {
+        }
+        if (name.substring(1).equals(name.substring(1).toLowerCase())) {
+            return "Witaj " + name;
+
+        } else return "WITAJ " + name.toUpperCase() + "!";
+
     }
 }
