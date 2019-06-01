cb9c9398de064ec3f2eb1f97efc30726231d037a
Mariusz Kowalczuk <lordmer@gmail.com>
2019-06-01 14:32:31 +0200

    zad 1

---
+++ src/main/java/pl/sda/demo/Greetings.java
@@ -0,0 +1,11 @@
+package pl.sda.demo;
+
+/**
+ * @author Mariusz Kowalczuk
+ */
+public class Greetings {
+
+    public String greet(String name){
+        return "Witaj " + name ;
+    }
+}
---
+++ src/test/java/pl/sda/demo/GreetingsTest.java
@@ -0,0 +1,25 @@
+package pl.sda.demo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Mariusz Kowalczuk
+ */
+public class GreetingsTest {
+
+    @Test
+    public void greet() {
+        //given
+        Greetings greetings = new Greetings();
+        String name = "Ala";
+
+        //when
+        String result = greetings.greet(name);
+
+        //then
+        Assert.assertEquals("Witaj Ala", result);
+    }
+}
